aa1bcb6f2d705725af00fa486a40e967cf88f473
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-04 05:18:07 -0400

    Remove unused includes and move the AAL

--- Source/arch/IntelPC/32/main.c
+++ Source/arch/IntelPC/32/main.c
@@ -1,26 +1,6 @@
-#include <Device/Terminal/Serial.h>
 #include <Device/VGA/ColorTextMode.h>
-#include <Device/VGA/Cursor.h>
 #include <Device/Terminal/Console.h>
-#include <Device/CPU/GDT.h>
-#include <Shared/Options.h>
-#include <Die.h>
-#include <Device/CPU/IDT.h>
-#include <Device/CPU/PIC.h>
-#include <Device/CPU/PIT.h>
-#include <Device/Terminal/Keyboard.h>
-#include <Device/FloppyController/Floppy.h>
-#include <Device/ISA/PnP/IO.h>
-#include <Log.h>
-#include <DAL.h>
-#include <stddef.h>
-#include <Device/PCI/PCI.h>
-#include <Device/ACPI/ACPI.h>
 #include "main.h"
-#include <Device/Memory.h>
-#include <AAL.h>
-#include <Device/Terminal/Serial.h>
-#include <VFS.h>
 #include <KernelMain.h>
 
 void SetupTextMode()
@@ -34,26 +14,6 @@ void SetupVideo()
 	SetupTextMode();
 	DebugConsole = NullConsole;
 }
-
-void ArchInit(DALDevice* Device)
-{
-	RegisterDALDeviceChild(ArchDevice, PICDevice, MessageCallback);
-	IDTInstall();
-	RegisterDALDeviceChild(ArchDevice, KeyboardDevice, MessageCallback);
-	RegisterDALDeviceChild(ArchDevice, PITDevice, MessageCallback);
-	__asm ("sti");
-	Device->SendKrnMessage(MsgDevReady, Device);
-}
-
-DALDevice* ArchDevice = &(DALDevice)
-{
-	.Name = "arch",
-	.Bus = DeviceBusCPU,
-	.Type = 0,
-	.Uninitialize = 0,
-	.Initialize = ArchInit,
-	.Command = 0
-};
 
 void Loader() {
 	SetupVideo();
