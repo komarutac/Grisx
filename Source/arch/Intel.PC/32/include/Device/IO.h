3d12ebb2b9007e22e1d6cb6401bba4700602c714
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-19 18:12:08 -0500

    Move inb, inw, and inl back to functions

--- Source/arch/Intel.PC/32/include/Device/IO.h
+++ Source/arch/Intel.PC/32/include/Device/IO.h
@@ -3,10 +3,11 @@
 #define outb(port, val) __asm volatile ("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory")
 #define outl(port, val) __asm volatile ("outl %k0, %w1" : : "a"(val), "Nd"(port) : "memory")
 #define outw(port, val) __asm volatile ("outw %w0, %w1" : : "a"(val), "Nd"(port) : "memory")
-#define inb(Port, A) __asm volatile ("inb %w1, %b0" : "=a"(A) : "Nd"(Port) : "memory")
-#define inw(Port, A) __asm volatile ("inw %w1, %w0" : "=a"(A) : "Nd"(Port) : "memory")
-#define inl(Port, A) __asm volatile ("inl %w1, %k0" : "=a"(A) : "Nd"(Port) : "memory")
 #define IOWait() outb(0x80, 0)
+
+extern uint8_t inb(int Port);
+extern uint16_t inw(int Port);
+extern unsigned long inl(int Port);
 
 struct _CommandQueueEntry {
 	uint16_t Port;
