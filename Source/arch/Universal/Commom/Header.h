a4bbf5bf0e29bf193a8dc89a3372389d66d9c5cc
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-18 20:33:03 -0500

    Add CommonMessageHandler and CommonTransferHeader

---
+++ Source/arch/Universal/Commom/Header.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stddef.h>
+
+struct _CommonTransferHeader
+{
+	int Context;
+	int Operation;
+	size_t DataSize;
+	size_t HeaderSize;
+}; typedef struct _CommonTransferHeader CommonTransferHeader;
+
+struct _CommonMessageHandler
+{
+	int RequestNumber;
+	size_t HeaderSize;
+	size_t DataSize;
+}; typedef struct _CommonMessageHandler CommonMessageHandler;
