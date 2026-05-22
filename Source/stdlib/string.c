b0ee3032f50ad7b224528d27efb1cdceea20a238
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-21 19:05:17 -0500

    Change style of string.c

--- Source/stdlib/string.c
+++ Source/stdlib/string.c
@@ -5,11 +5,13 @@
 size_t strlen(char* String)
 {
     size_t Length = 0;
+
     while (*String != '\0')
     {
         Length++;
         String++;
     }
+    
     return Length;
 }
 
@@ -19,22 +21,34 @@ int strcmp(char* A, char* B)
     return 0;
 }
 
-void* memset(void* Pointer, size_t Value, size_t Size) {
+void* memset(void* Pointer, size_t Value, size_t Size)
+{
     uint8_t* Buffer = (uint8_t*)Pointer;
-    for (size_t i = 0; i < Size; i++) {
+
+    for (size_t i = 0; i < Size; i++)
+    {
         Buffer[i] = (uint8_t)Value;
     }
+
     return Pointer;
 }
 
-int memcmp(const void* APointer, const void* BPointer, size_t Length) {
-    const uint8_t* a = (const uint8_t*)APointer;
-    const uint8_t* b = (const uint8_t*)BPointer;
-    for (size_t i = 0; i < Length; i++) {
-        if (a[i] < b[i])
+int memcmp(const void* APointer, const void* BPointer, size_t Length)
+{
+    const uint8_t* A = (const uint8_t*)APointer;
+    const uint8_t* B = (const uint8_t*)BPointer;
+
+    for (size_t i = 0; i < Length; i++)
+    {
+        if (A[i] < B[i])
+        {
             return -1;
-        else if (b[i] < a[i])
+        }
+        else if (B[i] < A[i])
+        {
             return 1;
+        }
     }
+
     return 0;
 }
