0d0dd814a7e7c038fd748193e3e2b657ef24fa30
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-22 02:50:51 -0500

    Add strncmp to string.h

--- Source/stdlib/string.h
+++ Source/stdlib/string.h
@@ -3,5 +3,6 @@
 #include "stddef.h"
 extern size_t strlen(char* String);
 extern int strcmp(char* A, char* B);
+extern int strncmp(char* A, char* B, size_t Length);
 extern void* memset(void* Pointer, size_t Value, size_t Size);
 extern int memcmp(const void* APointer, const void* BPointer, size_t Length);
