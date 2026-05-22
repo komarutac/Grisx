769d6eb21632ab2c21a7c90042c7019c52558135
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-22 04:38:55 -0500

    Add noreturn to Stop function

--- Source/arch/Universal/Die.h
+++ Source/arch/Universal/Die.h
@@ -3,8 +3,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <macro.h>
+#include <stdnoreturn.h>
 
 extern dependent void DieFault(char* Message, Registers* Regs, bool Die);
 extern independent void Oops(char* Reason, char* What);
 extern independent void Alert(char* Reason, char* What);
-extern independent void Stop(char* Message, uint32_t Code);
+extern independent noreturn void Stop(char* Message, uint32_t Code);
