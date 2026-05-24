391f436fe70940b910d8db103aefc44adac31417
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-24 16:03:51 -0500

    Add detect toolchain to Autoconfig

--- Source/Autoconfig.mk
+++ Source/Autoconfig.mk
@@ -1,25 +1,25 @@
-EDITION=base
-ifeq ($(HOSTARCH), x86_64)
-include Profile/intel.pc.64.base.uefi.mk
-endif
-ifeq ($(HOSTARCH), amd64)
-include Profile/intel.pc.64.base.uefi.mk
-endif
-ifeq ($(HOSTARCH), i386)
-include Profile/intel.pc.32.base.multiboot.mk
-endif
-ifeq ($(HOSTARCH), i686)
-include Profile/intel.pc.32.base.multiboot.mk
-endif
-ifeq ($(HOSTARCH), i686-AT386)
-include Profile/intel.pc.32.base.multiboot.mk
-endif
-ifeq ($(HOSTARCH), Power Macintosh)
-include Profile/powerpc.of.32.applemac.of.mk
-endif
-ifeq ($(HOSTARCH), x86)
-include Profile/intel.pc.32.base.multiboot.mk
-endif
 ifeq ($(HOSTOS), Darwin)
 EDITION=applemac
+else
+EDITION=base
+endif
+
+ifneq ($(shell which clang),)
+CC=clang
+endif
+
+include Profile/intel.pc.32.base.multiboot.mk
+
+ifeq ($(CC),)
+ifneq ($(shell which $(ARCHAKA)-$(OUTPUTFORMAT)-gcc),)
+CC=$(ARCHAKA)-$(OUTPUTFORMAT)-gcc
+endif
+endif
+
+ifneq ($(shell which nasm),)
+AS=nasm
+else
+ifneq ($(shell which yasm),)
+AS=yasm
+endif
 endif
