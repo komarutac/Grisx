c5ed4ec0d67888fa6135c450739cc02ac6b5f0eb
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-24 13:37:46 -0500

    Separate Intel.PC/32 PCI.c into multiple c files

--- Source/arch/Intel.PC/32/include/Device/PCI/PCIReadByte.c
+++ Source/arch/Intel.PC/32/include/Device/PCI/PCIReadByte.c
@@ -2,21 +2,6 @@
 #include <PCI.h>
 #include <Device/IO.h>
 
-uint16_t PCIReadConfig(uint8_t Bus, uint8_t Slot, uint8_t Function, uint8_t Offset)
-{
-	uint32_t Address, Bus32, Slot32, Function32;
-	uint16_t Result;
-	unsigned long Temp;
-	Bus32 = (uint32_t) Bus;
-	Slot32 = (uint32_t) Slot;
-	Function32 = (uint32_t) Function;
-	Address = (uint32_t) (Bus32 << 16) | (Slot32 << 11) | (Function32 << 8) | (Offset & 0xFC) | (uint32_t) 0x80000000;
-	outl(0xCF8, Address);
-	Temp = inl(0xCFC);
-	Result = (uint32_t) (Temp >> ((Offset & 2) * 8)) & 0xFFFF;
-	return Result;
-}
-
 uint8_t PCIReadByte(uint8_t Bus, uint8_t Slot, uint8_t Function, uint8_t Offset)
 {
 	uint32_t Address, Bus32, Slot32, Function32;
