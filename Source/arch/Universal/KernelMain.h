77142e17d367ac8a5f11adb256540d080195b995
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-24 10:42:27 -0500

    Add InitPCIEnum, InitPlatformDevices, InitVFS, and FindDiskCtrl to KernelMain.h

--- Source/arch/Universal/KernelMain.h
+++ Source/arch/Universal/KernelMain.h
@@ -1,2 +1,8 @@
 #pragma once
+#include <Abstraction/DAL.h>
+
 extern void KernelMain();
+extern void InitPCIEnum();
+extern void InitPlatformDevices();
+extern void InitVFS();
+extern DALDevice* FindDiskCtrl();
