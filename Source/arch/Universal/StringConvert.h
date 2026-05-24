73d9dcac63bb244186956acb73bbff923db68fb6
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-24 10:24:31 -0500

    Add SBase10

--- Source/arch/Universal/StringConvert.h
+++ Source/arch/Universal/StringConvert.h
@@ -3,6 +3,7 @@
 #include <stddef.h>
 
 extern char SBase16[];
+extern char SBase10[];
 extern size_t GetSizeForBase(int Integer, int Division);
 extern void strrev(int i, char* Destination);
 extern void IntegerToASCII(unsigned long Integer, char* Destination, char* Array, int Division);
