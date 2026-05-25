247bed2e45c6c254bb444abd6716deac51fb8182
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-24 21:47:59 -0500

    Add malloc, realloc, calloc, and free to stdlib.h

--- Source/stdlib/stdlib.h
+++ Source/stdlib/stdlib.h
@@ -1,3 +1,9 @@
 #pragma once
 #define ___stdlib_str(A) #A
 #define str(A) ___stdlib_str(A)
+#include <stddef.h>
+
+extern void* malloc(size_t Size);
+extern void* realloc(void* Pointer, size_t Size);
+extern void calloc(size_t Length, size_t Size);
+extern void free(void* Pointer);
