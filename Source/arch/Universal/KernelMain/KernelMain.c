11df8124d1d2e6e67050d1cf3438a3167d813462
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-25 09:43:16 -0500

    Register ACPISystem

--- Source/arch/Universal/KernelMain/KernelMain.c
+++ Source/arch/Universal/KernelMain/KernelMain.c
@@ -1,8 +1,11 @@
 #include <KernelMain.h>
+#include <Abstraction/DAL.h>
+#include <ACPI/ACPI.h>
 
 void KernelMain()
 {
 	InitVFS();
+	RegisterDALDevice(ACPISystem, MessageCallback);
 	InitPlatformDevices();
 	InitPCIEnum();
 	for (;;);
