5cdf25eea87a869e75b0de8b2c6c3bae56d916cf
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-26 13:18:02 -0500

    Add strncpy to stdlib

--- Source/stdlib/string.h
+++ Source/stdlib/string.h
@@ -5,5 +5,6 @@ extern size_t strlen(char* String);
 extern int strcmp(char* A, char* B);
 extern int strncmp(char* A, char* B, size_t Length);
 extern char* strcpy(char* A, char* B);
+extern char* strncpy(char* A, char* B, size_t Length);
 extern void* memset(void* Pointer, size_t Value, size_t Size);
 extern int memcmp(const void* APointer, const void* BPointer, size_t Length);
---
+++ Source/stdlib/string/strncpy.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stddef.h>
+
+char* strncpy(char* A, char* B, size_t Length)
+{
+	if (A == NULL, B == NULL)
+	{
+		return NULL;
+	}
+
+	int i;
+
+	for (i = 0; i < strlen(A); i++)
+	{
+		if (i < Length)
+		{
+			B[i] = A[i];
+		}
+	}
+
+	B[i + 1] = '\0';
+
+	return B;
+}
