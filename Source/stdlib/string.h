e32d94605a4750ab019b4584c1904a13760b76de
komarutac <118972777+komarutac@users.noreply.github.com>
2026-05-26 15:46:13 -0500

    Add memcpy

--- Source/stdlib/string.h
+++ Source/stdlib/string.h
@@ -8,3 +8,4 @@ extern char* strcpy(char* A, char* B);
 extern char* strncpy(char* A, char* B, size_t Length);
 extern void* memset(void* Pointer, size_t Value, size_t Size);
 extern int memcmp(const void* APointer, const void* BPointer, size_t Length);
+extern void* memcpy(void* APointer, void* BPointer, size_t Length);
---
+++ Source/stdlib/string/memcpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdint.h>
+#include <stddef.h>
+
+void* memcmp(void* APointer, void* BPointer, size_t Length)
+{
+    uint8_t* A = (const uint8_t*)APointer;
+    uint8_t* B = (const uint8_t*)BPointer;
+
+    for (size_t i = 0; i < Length; i++)
+    {
+        A[i] = B[i];
+    }
+
+    return A;
+}
