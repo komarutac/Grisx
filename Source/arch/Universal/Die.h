0f354947c6a28497ef7b62e3bc1317523c9bf78f
komarutac <118972777+komarutac@users.noreply.github.com>
2026-06-01 18:36:39 -0500

    Move noreturn to DieFault

--- Source/arch/Universal/Die.h
+++ Source/arch/Universal/Die.h
@@ -5,7 +5,7 @@
 #include <macro.h>
 #include <stdnoreturn.h>
 
-extern dependent void DieFault(char* Message, Registers* Regs, bool Die);
+extern dependent noreturn void DieFault(char* Message, Registers* Regs, bool Die);
 extern independent void Oops(char* Reason, char* What);
 extern independent void Alert(char* Reason, char* What);
-extern independent noreturn void Stop(char* Message, uint32_t Code);
+extern independent void Stop(char* Message, uint32_t Code);
