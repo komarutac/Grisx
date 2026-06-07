c68b48b7d6c4ca2116b27180beb29ec120f76282
komarutac <118972777+komarutac@users.noreply.github.com>
2026-06-06 20:43:16 -0500

    Fix -Wimplicit-function-declaration in Loader.c

--- Source/arch/Intel.PC/32/multiboot/Loader.c
+++ Source/arch/Intel.PC/32/multiboot/Loader.c
@@ -1,11 +1,12 @@
 #include <main.h>
 #include <Device/CPU/GDT.h>
 #include "Loader.h"
+#include <KernelMain.h>
 
 void Loader(uint32_t BootloaderMagic, LoaderInfo* Info) {
 	(void)BootloaderMagic;
 	(void)Info;
-	
+
 	GDTInstall();
 	SetupVideo();
 	KernelMain();
