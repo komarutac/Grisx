5650ae4d726dc0e0ac665c53dc00fea5bae29467
komarutac <118972777+komarutac@users.noreply.github.com>
2026-06-17 10:02:15 -0500

    Add multiboot headers

--- Source/arch/Intel.PC/32/multiboot/Loader.c
+++ Source/arch/Intel.PC/32/multiboot/Loader.c
@@ -2,12 +2,31 @@
 #include <Device/CPU/GDT.h>
 #include "Loader.h"
 #include <KernelMain.h>
+#include <Abstraction/Console.h>
 
-void Loader(uint32_t BootloaderMagic, LoaderInfo* Info) {
+void HandleModule(ModuleTable* Module)
+{
+	printf("Loading module %s...\r\n", Module->Name);
+}
+
+void Loader(uint32_t BootloaderMagic, MultibootHeader* Info) {
 	(void)BootloaderMagic;
 	(void)Info;
 
 	GDTInstall();
 	SetupVideo();
+
+	printf("Loading %d module(s)...\r\n", Info->ModuleCount);
+
+	uint32_t Address = Info->ModuleAddress;
+	ModuleTable* Module = (ModuleTable*)Address;
+
+	for (uint32_t i = 0; i < Info->ModuleCount; i++)
+	{
+		HandleModule(Module);
+		Module = (ModuleTable*)(Address + Module->End);
+	}
+
+	printf("Starting kernel...");
 	KernelMain();
 }
--- Source/arch/Intel.PC/32/multiboot/Loader.h
+++ Source/arch/Intel.PC/32/multiboot/Loader.h
@@ -1,13 +1,32 @@
 #pragma once
 #include <stdint.h>
 
-struct _LoaderInfo
+struct _ElfHeaderTable
+{
+	uint32_t Number;
+	uint32_t Size;
+	uint32_t Address;
+	uint32_t SectionIndex;
+}; typedef struct _ElfHeaderTable ElfHeaderTable;
+
+struct _ModuleTable
+{
+	uint32_t Start;
+	uint32_t End;
+	uint32_t Name;
+	uint32_t Reserved;
+}; typedef struct _ModuleTable ModuleTable;
+
+struct _MultibootHeader
 {
 	uint32_t Flags;
-	uint32_t UpperMemory;
 	uint32_t LowerMemory;
+	uint32_t UpperMemory;
 	uint32_t BootDevice;
-	uint32_t Arguments;
+	uint32_t CommandLine;
 	uint32_t ModuleCount;
 	uint32_t ModuleAddress;
-}; typedef struct _LoaderInfo LoaderInfo;
+	ElfHeaderTable Elf;
+	uint32_t MemoryMapLength;
+	uint32_t MemoryMapAddress;
+}; typedef struct _MultibootHeader MultibootHeader;
