7964964b1087651c5d8239c173526db969054e36
komarutac <118972777+komarutac@users.noreply.github.com>
2026-06-17 20:40:14 -0500

    Show physical  memory information

--- Source/arch/Intel.PC/32/multiboot/Loader.c
+++ Source/arch/Intel.PC/32/multiboot/Loader.c
@@ -4,6 +4,8 @@
 #include <KernelMain.h>
 #include <Abstraction/Console.h>
 
+MultibootHeader* Multiboot;
+
 void HandleModule(ModuleTable* Module)
 {
 	printf("Loading module %s...\r\n", Module->Name);
@@ -12,10 +14,65 @@ void HandleModule(ModuleTable* Module)
 void Loader(uint32_t BootloaderMagic, MultibootHeader* Info) {
 	(void)BootloaderMagic;
 	(void)Info;
+	Multiboot = Info;
 
 	GDTInstall();
 	SetupVideo();
 
+	uint32_t FreeMemory = 0;
+	uint32_t BadMemory = 0;
+	uint32_t ReclaimableMemory = 0;
+	uint32_t UsedMemory = 0;
+	uint32_t Entries = 0;
+
+	for (uint32_t i = Info->MemoryMapAddress; i < Info->MemoryMapAddress + Info->MemoryMapLength; i += 4)
+	{
+		uint32_t Byte = *(uint32_t*)i;
+
+		if (Byte == 0x14)
+		{
+			MemoryMapEntry* MemMap = (MemoryMapEntry*)i;
+
+			if (MemMap == 0)
+			{
+				continue;
+			}
+
+			if (MemMap->Address == 0 && MemMap->Length == 0)
+			{
+				continue;
+			}
+
+			if (MemMap->Type == MemoryMapTypeFree)
+			{
+				FreeMemory += MemMap->Length;
+			}
+
+			if (MemMap->Type == MemoryMapTypeBad)
+			{
+				BadMemory += MemMap->Length;
+			}
+
+			if (MemMap->Type == MemoryMapTypeReclaimable)
+			{
+				ReclaimableMemory += MemMap->Length;
+			}
+
+			if (MemMap->Type == MemoryMapTypeUsed)
+			{
+				UsedMemory += MemMap->Length;
+			}
+
+			Entries++;
+			i += sizeof(MemoryMapEntry) - 4;
+		}
+	}
+
+	printf("Free physical memory right now: %d kbytes\r\n", FreeMemory / 1024);
+	printf("Bad physical memory right now: %d kbytes\r\n", BadMemory / 1024);
+	printf("Reclaimable physical memory right now: %d kbytes\r\n", ReclaimableMemory / 1024);
+	printf("Used physical memory right now: %d kbytes\r\n", UsedMemory / 1024);
+	printf("Physical memory entries right now: %d\r\n", Entries);
 	printf("Loading %d module(s)...\r\n", Info->ModuleCount);
 
 	uint32_t Address = Info->ModuleAddress;
--- Source/arch/Intel.PC/32/multiboot/Loader.h
+++ Source/arch/Intel.PC/32/multiboot/Loader.h
@@ -1,5 +1,11 @@
 #pragma once
 #include <stdint.h>
+#define MemoryMapTypeFree 1
+#define MemoryMapTypeUsed 2
+#define MemoryMapTypeReclaimable 3
+#define MemoryMapTypeNVS 4
+#define MemoryMapTypeBad 5
+#define MemoryMapTypeProgramUsed 6
 
 struct _ElfHeaderTable
 {
@@ -17,6 +23,16 @@ struct _ModuleTable
 	uint32_t Reserved;
 }; typedef struct _ModuleTable ModuleTable;
 
+struct _MemoryMapEntry
+{
+	uint32_t Size;
+	uint32_t Address;
+	uint32_t _;
+	uint32_t Length;
+	uint32_t __;
+	uint32_t Type;
+} __attribute__((packed)); typedef struct _MemoryMapEntry MemoryMapEntry;
+
 struct _MultibootHeader
 {
 	uint32_t Flags;
@@ -30,3 +46,5 @@ struct _MultibootHeader
 	uint32_t MemoryMapLength;
 	uint32_t MemoryMapAddress;
 }; typedef struct _MultibootHeader MultibootHeader;
+
+extern MultibootHeader* Multiboot;
