259fab5bb76e116694d2d5c1a5025129bc295a13
komarutac <118972777+komarutac@users.noreply.github.com>
2026-06-19 12:15:39 -0500

    Forgot to include this with the prev commit

--- Source/arch/Universal/Build/Linker.h
+++ Source/arch/Universal/Build/Linker.h
@@ -1,3 +1,8 @@
 #pragma once
 extern unsigned char EndProgram;
 extern unsigned char StartProgram;
+extern unsigned char EndText;
+extern unsigned char StartText;
+extern unsigned char EndText;
+extern unsigned char StartAllData;
+extern unsigned char EndAllData;
