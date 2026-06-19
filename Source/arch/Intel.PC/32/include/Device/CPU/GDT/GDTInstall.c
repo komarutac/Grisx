36ce3cd234ecdbce81d1d8da6f27188ec3e86115
komarutac <118972777+komarutac@users.noreply.github.com>
2026-06-18 21:00:15 -0500

    Make GDTInstall more readable

--- Source/arch/Intel.PC/32/include/Device/CPU/GDT/GDTInstall.c
+++ Source/arch/Intel.PC/32/include/Device/CPU/GDT/GDTInstall.c
@@ -6,7 +6,7 @@ void GDTInstall()
     GP.Limit = (sizeof(GDTEntry) * 3) - 1;
     GP.Base = (uint32_t)&GDT;
     GDTSetGate(0, 0, 0, 0, 0);
-    GDTSetGate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
-    GDTSetGate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+    GDTSetGate(1, 0, 0xFFFFFFFF, 1 << 7 | 0 << 5 | 1 << 4 | 10, 1 << 7 | 1 << 6 | 15);
+    GDTSetGate(2, 0, 0xFFFFFFFF, 1 << 7 | 0 << 5 | 1 << 4 | 2, 1 << 7 | 1 << 6 | 15);
     GDTFlush();
 }
